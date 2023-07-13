6980c7405d0e12385f8dd2b21ffe42f2d41fb6c5
mehmet ali <maliyildiz0307@gmail.com>
2023-07-13 14:45:00 +0300

    some folder created

---
+++ src/Pages/introduce/introduce.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./introduce.css"
+
+function introduce() {
+  return (
+    <div className='introduce'>
+        introduce
+    </div>
+  )
+}
+
+export default introduce
---
+++ src/Pages/quiz/quiz.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./quiz.css"
+
+function quiz() {
+  return (
+    <div className='quiz'>quiz</div>
+  )
+}
+
+export default quiz
